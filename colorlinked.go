1c6ef5dc446c805f739ca3113c6f6e0fc540b917
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-28 14:34:14 -0700

    docs

--- colorlinked.go
+++ colorlinked.go
@@ -12,6 +12,9 @@ type ColorLinked struct {
 	Color color.Color
 }
 
+// At returns the color at an (x,y) coordinate.
+//
+// At helps ColorLinked  fit the Go built-in image.Image interface.
 func (receiver ColorLinked) At(x, y int) color.Color {
 	if receiver.X != x || receiver.Y != y {
 		return RGBA{
@@ -23,6 +26,9 @@ func (receiver ColorLinked) At(x, y int) color.Color {
 	return receiver
 }
 
+// Bounds returns the (rectangular) area for which RGBA may have non-zero color.
+//
+// Bounds helps ColorLinked fit the Go built-in image.Image interface.
 func (receiver ColorLinked) Bounds() image.Rectangle {
 	const width  = 1
 	const height = 1
@@ -39,10 +45,16 @@ func (receiver ColorLinked) Bounds() image.Rectangle {
 	}
 }
 
+// ColorModel returns the color-model.
+//
+// ColorModel helps ColorLinked fit the Go built-in image.Image interface.
 func (receiver ColorLinked) ColorModel() color.Model {
 	return color.NRGBAModel
 }
 
+// RGBA returns the alpha-premultiplied red, green, blue and alpha values for the color.
+//
+// Bounds helps ColorLinked fit the Go built-in color.Color interface.
 func (receiver ColorLinked) RGBA() (r, g, b, a uint32) {
 	return receiver.Color.RGBA()
 }
