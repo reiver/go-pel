cbfd076106b5f4c7ec395270ccc386af04ce3102
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-28 10:15:28 -0700

    docs

--- doc.go
+++ doc.go
@@ -2,6 +2,6 @@
 Package pel provides a type that represents a single pel — that represents a single pixel — at a specific (x,y) location,
 that seamlessly works with Go's built-in "image", "image/color", and "image/draw" packages.
 
-A pel is also often called a pixel.
+A ‘pel’ is also often called a ‘pixel’.
 */
 package pel
